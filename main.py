6d20ca1926492216ab14294f4734eb6978f8f68f
AntoineLafosse <552711@my.cuhsd.org>
2020-10-16 18:59:24 +0000

    added draw axis function which draws the x and y axis

--- main.py
+++ main.py
@@ -3,3 +3,16 @@ for i in range (6):
   trtl.forward(10)
   trtl.left(15)
 trtl.penup()
+
+trtl.pendown()
+def draw_axis ():
+  trtl.penup()
+  trtl.goto(0,0)
+  trtl.pendown()
+  trtl.forward(100)
+  trtl.backward(200)
+  trtl.forward(100)
+  trtl.left(90)
+  trtl.forward(100)
+  trtl.backward(200)
+draw_axis()
